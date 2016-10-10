9bc1d51166a9b649e64ea3a4507f3cf4b7d94cde
Justin D. Forman <jforman@ece015.campus.ece.cmu.local>
2016-10-10 16:13:32 -0400

    Clean up testbench files and update .gitignore

--- z80/tb/traces/test0.asm
+++ z80/tb/traces/test0.asm
@@ -5,18 +5,13 @@ range data $0051 $0100
 
 
 section code
-  #ld b, $dd
-  #ld c, $ee
-  #ld d, $ff
-  #ld e, $aa
-  #ld a, [st+$51]
-  #ld b, [uv+$52]
+  ld b, $ff
+  ld [st + $51], b
+  ld c, [st + $51]
 
-  ld h, $00
-  ld l, $51
-  ld d, $ff
-  ld [hl], d      #[$51] <- d
-  ld e, [st+$51]  # e <- [$51]
+  ld d, $ee
+  ld [uv + $52], d
+  ld e, [uv + $52]
 
 section data
   addr0: data !DEAD !BEEF
