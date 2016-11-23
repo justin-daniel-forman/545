70cbcc536efd31ba94af9f56a8ccdd5a66972e8e
Justin D. Forman <jforman@ece015.campus.ece.cmu.local>
2016-11-23 13:01:24 -0500

    Merge top

--- z80/tb/traces/sbc_hl_test.asm
+++ z80/tb/traces/sbc_hl_test.asm
@@ -5,14 +5,16 @@ range data $0051 $0100
 
 
 section code
-  #compute a 16-bit subtraction
-  #98ba - 98bb -1  = fffe (-2)
 
-  stc #set the carry bit
-  #part 1 98ba - 98bb - C_in = fffe
-  ld hl $98ba
-  ld bc $98bb
-  sbc hl bc
+  or a #clear flags
+  ld hl, $0028
+  ld de, $0001
+  sbc hl, de
+
+  #or a 
+  #ld hl, $0028
+  #ld de, $0028
+  #sbc hl, de
 
 section data
   addr0: data !ba98 !bb98 !0c00 !0c00 !3800 !7000
